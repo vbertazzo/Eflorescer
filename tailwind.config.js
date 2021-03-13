cd67d5afc50cd91f0cd8c1f32bb1327dc720fd32
Vitor Bertazzo <vbertazzo@gmail.com>
2021-03-13 02:43:02 -0300

    Add custom colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
 	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary: '#FCD5CE',
+				'primary-light': '#F8F2F1',
+				secondary: '#7A6A67',
+				tertiary: '#E8E8E4'
+			}
+		}
 	},
 	variants: {
 		extend: {}
