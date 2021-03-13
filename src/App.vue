15baa8550fa5b862b3cb199b266eb719a96db11a
Vitor Bertazzo <vbertazzo@gmail.com>
2021-03-13 13:50:14 -0300

    Add hover effect to nav icons

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,8 @@
 </template>
 
 <script setup>
-import { defineComponent } from 'vue'
 import TheHeader from './components/layout/TheHeader.vue'
 
-defineComponent([TheHeader])
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -3,32 +3,71 @@
 		class="w-full h-16 px-3 flex justify-center items-center bg-primary border-b-4 border-secondary"
 	>
 		<nav class="w-11/12 m-auto flex justify-between items-center">
-			<img :src="Logo" alt="Eflorescer Logo" />
+			<router-link to="/">
+				<img
+					:src="Logo"
+					alt="Eflorescer Logo"
+					title="Eflorescer - Chás e Infusões"
+				/>
+			</router-link>
 			<ul class="flex justify-center items-center space-x-8">
 				<li class="text-secondary">
-					<img :src="UserIcon" alt="Abrir perfil" />
+					<router-link to="/perfil" title="Abrir perfil">
+						<svg
+							class="h-6 w-6 text-secondary hover:text-green-500"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+							/>
+						</svg>
+					</router-link>
 				</li>
-				<li class="text-secondary">
-					<img :src="ShoppingBagIcon" alt="Abrir carrinho" />
+				<li>
+					<router-link to="/carrinho" title="Abrir carrinho">
+						<svg
+							class="h-6 w-6 text-secondary hover:text-green-500"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+							/>
+						</svg>
+					</router-link>
 				</li>
-				<li class="text-secondary">
-					<img :src="MenuIcon" alt="Abrir menu" />
+				<li class="text-secondary cursor-pointer" title="Abrir menu">
+					<svg
+						class="h-6 w-6 text-secondary hover:text-green-500"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h16M4 18h16"
+						/>
+					</svg>
 				</li>
 			</ul>
 		</nav>
 	</header>
 </template>
 
-<script>
+<script setup>
 import Logo from '../../assets/Logo.svg'
-import UserIcon from '../../assets/User.svg'
-import MenuIcon from '../../assets/Menu.svg'
-import ShoppingBagIcon from '../../assets/ShoppingBag.svg'
-
-export default {
-	components: [Logo, MenuIcon, UserIcon, ShoppingBagIcon],
-	setup () {
-		return { Logo, MenuIcon, UserIcon, ShoppingBagIcon }
-	}
-}
 </script>
