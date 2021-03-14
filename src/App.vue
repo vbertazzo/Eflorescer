dce5a446328d1da503f2bcdf0fc459528b52c60a
Vitor Bertazzo <vbertazzo@gmail.com>
2021-03-13 23:53:25 -0300

    Add fun fact component

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 	<div>
 		<the-header></the-header>
 		<the-hero></the-hero>
+		<fun-fact></fun-fact>
 	</div>
 </template>
 
 <script setup>
 import TheHeader from './components/layout/TheHeader.vue'
 import TheHero from './components/layout/TheHero.vue'
+import FunFact from './components/FunFact.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
---
+++ src/components/FunFact.vue
@@ -0,0 +1,19 @@
+<template>
+	<section class="my-6 mx-6 flex flex-col justify-center items-center">
+		<img
+			class="mx-8 w-60 object-contain"
+			src="../assets/TeaDraw@2x.png"
+			alt="Amigas bebendo chá"
+		/>
+		<div class="mx-8 mt-8 flex flex-col justify-center ">
+			<h3 class="text-xl font-serif">Você sabia?</h3>
+			<p class="mt-2 text-sm font-light tracking-wider">
+				O Rooibos tem-se tornado popular devido ao seu alto nível de
+				antioxidantes, por não conter cafeína e por seus baixos níveis de
+				tanino, comparados com aqueles do chá preto tostado ou do chá verde.
+			</p>
+		</div>
+	</section>
+</template>
+
+<script setup></script>
