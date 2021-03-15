76d0d46f6da0761eec152df0127e81429455c87e
Vitor Bertazzo <vbertazzo@gmail.com>
2021-03-15 01:29:40 -0300

    Add vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import router from './router'
+import store from './store/index'
 import App from './App.vue'
 import './index.css'
 
 createApp(App)
 	.use(router)
+	.use(store)
 	.mount('#app')
---
+++ src/store/auth/index.js
@@ -0,0 +1,12 @@
+import actions from './actions'
+import getters from './getters'
+import mutations from './mutations'
+
+export default {
+	state () {
+		return {}
+	},
+	actions,
+	getters,
+	mutations
+}
