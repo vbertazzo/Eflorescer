ca824f993fd58c3022200fbd18951779e3247aed
Vitor Bertazzo <vbertazzo@gmail.com>
2021-03-15 01:49:14 -0300

    Add base button component

---
+++ src/components/ui/BaseButton.vue
@@ -0,0 +1,30 @@
+<template>
+	<button v-if="!link" :class="mode">
+		<slot></slot>
+	</button>
+	<router-link v-else :to="to" :class="mode">
+		<slot></slot>
+	</router-link>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+	mode: {
+		type: String,
+		required: false,
+		default: null
+	},
+	link: {
+		type: Boolean,
+		required: false,
+		default: false
+	},
+	to: {
+		type: String,
+		required: false,
+		default: '/'
+	}
+})
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
 import router from './router'
 import store from './store/index'
+
 import App from './App.vue'
+import BaseButton from './components/ui/BaseButton.vue'
+
 import './index.css'
 
 createApp(App)
 	.use(router)
 	.use(store)
+	.component('base-button', BaseButton)
 	.mount('#app')
