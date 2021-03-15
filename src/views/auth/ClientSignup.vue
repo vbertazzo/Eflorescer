df1fcf5529109f0ebb51b30e210b6b3c0ef8fb99
Vitor Bertazzo <vbertazzo@gmail.com>
2021-03-15 01:49:40 -0300

    Add client signup view

---
+++ src/views/auth/ClientSignup.vue
@@ -0,0 +1,20 @@
+<template>
+	<form>
+		<h2>Registrar</h2>
+		<div>
+			<label for="username"></label>
+			<input type="text" id="username" />
+		</div>
+		<div>
+			<label for="password"></label>
+			<input type="password" id="password" />
+		</div>
+		<div>
+			<label for="confirm-password"></label>
+			<input type="password" id="confirm-password" />
+		</div>
+		<base-button>Registrar</base-button>
+	</form>
+</template>
+
+<script setup></script>
