26f1e9a8423d01228a70ced0ab9fcb973ab3243f
Vitor Bertazzo <vbertazzo@gmail.com>
2021-03-15 01:09:56 -0300

    Add footer component

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 		<the-hero></the-hero>
 		<product-list></product-list>
 		<fun-fact></fun-fact>
+		<the-footer></the-footer>
 	</div>
 </template>
 
@@ -12,6 +13,7 @@ import TheHeader from './components/layout/TheHeader.vue'
 import TheHero from './components/layout/TheHero.vue'
 import ProductList from './components/products/ProductList.vue'
 import FunFact from './components/FunFact.vue'
+import TheFooter from './components/layout/TheFooter.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
